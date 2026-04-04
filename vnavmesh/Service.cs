dce54cd5352658c5eab65e1049a569b6e958cf32
AtmoOmen <atmoomen2004@outlook.com>
2026-04-04 17:01:16 +0800

    简化 Row 获取

--- vnavmesh/Service.cs
+++ vnavmesh/Service.cs
@@ -25,8 +25,7 @@ public class Service
     [PluginService] public static IGameConfig             GameConfig      { get; private set; } = null!;
 
     public static Config Config { get; set; } = null!;
-    
-    public static Lumina.GameData LuminaGameData => DataManager.GameData;
-    public static Lumina.Excel.ExcelSheet<T>? LuminaSheet<T>() where T : struct, Lumina.Excel.IExcelRow<T> => LuminaGameData?.GetExcelSheet<T>(Lumina.Data.Language.English);
-    public static T? LuminaRow<T>(uint row) where T : struct, Lumina.Excel.IExcelRow<T> => LuminaSheet<T>()?.GetRowOrDefault(row);
+
+    public static T? LuminaRow<T>(uint row) where T : struct, Lumina.Excel.IExcelRow<T> => 
+        DataManager.GameData.GetExcelSheet<T>()?.GetRowOrDefault(row);
 }
