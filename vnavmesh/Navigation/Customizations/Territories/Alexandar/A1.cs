3dc5008357c96d54ab7c11091cef61c6444350f7
AtmoOmen <atmoomen2004@outlook.com>
2026-05-10 00:57:24 +0800

    A1

--- vnavmesh/Navigation/Customizations/Territories/Alexandar/A1.cs
+++ vnavmesh/Navigation/Customizations/Territories/Alexandar/A1.cs
@@ -12,12 +12,14 @@ internal class A1 : NavmeshCustomization
 
     public override void CustomizeMesh(Navmesh mesh, List<uint> festivalLayers)
     {
+        // 喷气
         LinkPoints(mesh, new(3.2f, -7.3f, 20.3f), new(2.6f, -4.2f, 3.0f));
         LinkPoints(mesh, new(2.6f, -4.2f, 3.0f),  new(-4.6f, -3.4f, -1.9f));
     }
     
      public override void CustomizeSettings(DtNavMeshCreateParams config)
     {
+        // 传送带
         config.AddOffMeshConnection(new(0.1f, 12.0f, -106.5f), new(-0.0f, -23.9f, -148.1f));
     }
 }
