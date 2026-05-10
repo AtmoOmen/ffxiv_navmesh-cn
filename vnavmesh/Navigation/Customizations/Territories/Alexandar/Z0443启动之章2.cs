54c5f115bc932106eaa962fa1c5938fd93a95676
AtmoOmen <atmoomen2004@outlook.com>
2026-05-10 22:39:11 +0800

    两个自定义

--- vnavmesh/Navigation/Customizations/Territories/Alexandar/Z0443启动之章2.cs
+++ vnavmesh/Navigation/Customizations/Territories/Alexandar/Z0443启动之章2.cs
@@ -1,5 +1,8 @@
-﻿using vnavmesh.Common.Navigation.Mesh.Runtime;
+﻿using DotRecast.Detour;
+using vnavmesh.Common.Navigation.Mesh.Runtime;
 using vnavmesh.Navigation.Customizations.Attributes;
+using vnavmesh.Navigation.Customizations.Extensions;
+using vnavmesh.Navigation.Mesh.Build;
 
 namespace vnavmesh.Navigation.Customizations.Territories.Alexandar;
 
@@ -8,6 +11,23 @@ internal class Z0443启动之章2 : NavmeshCustomization
 {
     public override int Version => 1;
     
+    // 得要能跳下去
+    public Z0443启动之章2() =>
+        Settings.Filtering -= NavmeshSettings.Filter.LedgeSpans;
+
     public override void CustomizeMesh(Navmesh mesh, List<uint> festivalLayers) =>
         LinkDrop(mesh, new(9.0f, 12.1f, 36.5f), new(-3.3f, -18.2f, 37.5f));
+    
+    public override void CustomizeSettings(DtNavMeshCreateParams config) =>
+        config.AddOffMeshConnection
+        (
+            new(9.0f, 12.1f, 36.5f), 
+            new(-3.3f, -18.2f, 37.5f),
+            0.5f,
+            true,
+            0,
+            NavmeshArea.GeneratedEdgeJump,
+            NavmeshPolyFlags.GeneratedEdgeJump,
+            NavmeshOffMeshKind.GeneratedEdgeJump
+        );
 }
--- vnavmesh/Navigation/Customizations/Territories/Overworld/Z0815安穆艾兰.cs
+++ vnavmesh/Navigation/Customizations/Territories/Overworld/Z0815安穆艾兰.cs
@@ -1,5 +1,9 @@
-﻿using vnavmesh.Common.Navigation.Mesh.Runtime;
+﻿using DotRecast.Detour;
+using vnavmesh.Common.Navigation.Mesh.Runtime;
 using vnavmesh.Navigation.Customizations.Attributes;
+using vnavmesh.Navigation.Customizations.Extensions;
+using vnavmesh.Navigation.Mesh.Build;
+using vnavmesh.Navigation.Scene;
 
 namespace vnavmesh.Navigation.Customizations.Territories.Overworld;
 
@@ -8,11 +12,34 @@ public class Z0815安穆艾兰 : NavmeshCustomization
 {
     public override int Version => 1;
 
+    public Z0815安穆艾兰()
+    {
+        // 因为新宫那边的地形太抽象了
+        Settings.AgentMaxSlopeDeg =  90f;
+        Settings.Filtering        -= NavmeshSettings.Filter.LedgeSpans;
+    }
+
     public override void CustomizeMesh(Navmesh mesh, List<uint> festivalLayers)
     {
         // 新宫
         LinkPoints(mesh, new(434.5f, -106.0f, 590.0f), new(422.7f, -105.3f, 589.6f));
-        
+
         LinkPoints(mesh, new(425.0f, -105.7f, 589.0f), new(434.7f, -105.5f, 591.1f));
+    }
+
+    public override void CustomizeSettings(DtNavMeshCreateParams config)
+    {
+        // 新宫
+        config.AddOffMeshConnection
+        (
+            new(425.0f, -105.7f, 589.0f),
+            new(434.7f, -105.5f, 591.1f),
+            0.5f,
+            true,
+            0,
+            NavmeshArea.ManualOffMesh,
+            NavmeshPolyFlags.AllTraversable,
+            NavmeshOffMeshKind.ManualOffMesh
+        );
     }
 }
