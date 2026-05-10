aa71183d22f19d3fcc6acf625c41fe7a0fb4a619
AtmoOmen <atmoomen2004@outlook.com>
2026-05-10 20:27:57 +0800

    安穆艾兰

--- vnavmesh/Navigation/Customizations/Territories/Overworld/Z0815安穆艾兰.cs
+++ vnavmesh/Navigation/Customizations/Territories/Overworld/Z0815安穆艾兰.cs
@@ -6,9 +6,13 @@ namespace vnavmesh.Navigation.Customizations.Territories.Overworld;
 [CustomizationTerritory(815)]
 public class Z0815安穆艾兰 : NavmeshCustomization
 {
+    public override int Version => 1;
+
     public override void CustomizeMesh(Navmesh mesh, List<uint> festivalLayers)
     {
         // 新宫
         LinkPoints(mesh, new(434.5f, -106.0f, 590.0f), new(422.7f, -105.3f, 589.6f));
+        
+        LinkPoints(mesh, new(425.0f, -105.7f, 589.0f), new(434.7f, -105.5f, 591.1f));
     }
 }
