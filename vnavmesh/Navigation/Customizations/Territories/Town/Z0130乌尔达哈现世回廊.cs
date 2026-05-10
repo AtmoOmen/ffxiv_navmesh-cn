3db7c114388652eb397c5795f565fb9fb7e535a9
AtmoOmen <atmoomen2004@outlook.com>
2026-05-11 01:47:46 +0800

    继续优化乌尔达哈的

--- vnavmesh/Navigation/Customizations/Territories/Town/Z0130乌尔达哈现世回廊.cs
+++ vnavmesh/Navigation/Customizations/Territories/Town/Z0130乌尔达哈现世回廊.cs
@@ -10,7 +10,7 @@ internal class Z0130乌尔达哈现世回廊 : NavmeshCustomization
 
     public override void CustomizeScene(SceneExtractor scene)
     {
-        //Force all instances of Mesh as unwalkable
+        // Force all instances of Mesh as unwalkable
         if (scene.Meshes.TryGetValue("bg/ffxiv/wil_w1/twn/common/collision/w1t0_f0_kadn1.pcb", out var mesh))
         {
             foreach (var instance in mesh.Instances)
--- vnavmesh/Navigation/Customizations/Territories/Town/Z0131乌尔达哈来生回廊.cs
+++ vnavmesh/Navigation/Customizations/Territories/Town/Z0131乌尔达哈来生回廊.cs
@@ -8,16 +8,6 @@ namespace vnavmesh.Navigation.Customizations.Territories.Town;
 internal class Z0131乌尔达哈来生回廊 : NavmeshCustomization
 {
     public override int Version => 2;
-
-    public override void CustomizeScene(SceneExtractor scene)
-    {
-        // Force all instances of Mesh as unwalkable
-        if (scene.Meshes.TryGetValue("bg/ffxiv/wil_w1/twn/common/collision/w1t0_f0_kadn1.pcb", out var mesh))
-        {
-            foreach (var instance in mesh.Instances)
-                instance.ForceSetPrimFlags |= SceneExtractor.PrimitiveFlags.ForceUnwalkable;
-        }
-    }
     
     public override void CustomizeMesh(Navmesh mesh, List<uint> festivalLayers)
     {
@@ -25,5 +15,7 @@ internal class Z0131乌尔达哈来生回廊 : NavmeshCustomization
         LinkPoints(mesh, new(-87.3f, 42.0f, 48.3f), new(-83.9f, 42.0f, 35.3f));
         
         LinkPoints(mesh, new(-22.7f, 30.0f, -6.6f), new(-68.6f, 37.9f, -18.6f));
+        
+        LinkPoints(mesh, new(62.0f, 29.0f, -36.3f), new(42.1f, 34.0f, -24.3f));
     }
 }
