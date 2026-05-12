9665e2a4ca06ec280583d1967ccb40d81e4b199e
AtmoOmen <atmoomen2004@outlook.com>
2026-05-12 19:32:49 +0800

    A3精修

--- vnavmesh/Navigation/Customizations/Territories/Alexandar/Z0444启动之章3.cs
+++ vnavmesh/Navigation/Customizations/Territories/Alexandar/Z0444启动之章3.cs
@@ -3,6 +3,7 @@ using FFXIVClientStructs.FFXIV.Common.Component.BGCollision.Math;
 using vnavmesh.Common.Navigation.Mesh.Runtime;
 using vnavmesh.Navigation.Customizations.Attributes;
 using vnavmesh.Navigation.Customizations.Extensions;
+using vnavmesh.Navigation.Mesh.Build;
 using vnavmesh.Navigation.Scene;
 
 namespace vnavmesh.Navigation.Customizations.Territories.Alexandar;
@@ -12,13 +13,12 @@ internal class Z0444启动之章3 : NavmeshCustomization
 {
     public override int Version => 2;
 
-    public Z0444启动之章3()
+    public override void CustomizeBuildSettings(SceneDefinition definition, NavmeshSettings settings)
     {
-        Settings.CellSize    = 0.25f;
-        Settings.CellHeight  = 0.125f;
-        Settings.AgentRadius = 0.5f;
-        Settings.AgentHeight = 2f;
-        Settings.FastBuild   = false;
+        settings.CellSize    = 0.25f;
+        settings.CellHeight  = 0.125f;
+        settings.AgentHeight = 1.5f;
+        settings.AgentRadius = 0.5f;
     }
 
     public override void CustomizeScene(SceneExtractor scene)
@@ -27,79 +27,61 @@ internal class Z0444启动之章3 : NavmeshCustomization
         (
             new AABB
             {
-                Min = new(-9.5f, 0.3f, 24.7f),
-                Max = new(7.7f, 5f, 20.1f)
+                Min = new(-17.823376f, -0.12699986f, 5.392f),
+                Max = new(19.035625f, 3.019f, 24.518f)
             },
             SceneExtractor.PrimitiveFlags.ForceUnwalkable
         );
-
         scene.InsertAABoxCollider
         (
             new AABB
             {
-                Min = new(9.9f, 18.0f, -16.1f),
-                Max = new(-1.2f, 20.0f, -37.1f)
+                Min = new(5.84f, -1.561f, 0.6084976f),
+                Max = new(26.05f, 3.361f, 38.433502f)
             },
             SceneExtractor.PrimitiveFlags.ForceUnwalkable
         );
-
         scene.InsertAABoxCollider
         (
             new AABB
             {
-                Min = new(25.3f, 0.1f, 8.9f),
-                Max = new(7.3f, -1.0f, 5.4f)
+                Min = new(-3.0751038f, 19.204956f, -33.202503f),
+                Max = new(12.815897f, 23.276955f, -19.051498f)
             },
             SceneExtractor.PrimitiveFlags.ForceUnwalkable
         );
+    }
 
-        scene.InsertAABoxCollider
+    public override void CustomizeSettings(DtNavMeshCreateParams config)
+    {
+        config.AddOffMeshConnection
         (
-            new AABB
-            {
-                Min = new(19.8f, -0.6f, -8.6f),
-                Max = new(12.2f, 4f, -24.8f)
-            },
-            SceneExtractor.PrimitiveFlags.ForceUnwalkable
+            new(16.790548f, 18f, -18.193995f),
+            new(20.274364f, 8.826821f, -23.925808f),
+            2f,
+            false,
+            0,
+            NavmeshArea.GeneratedEdgeJump,
+            NavmeshPolyFlags.GeneratedEdgeJump | NavmeshPolyFlags.ManualOffMesh,
+            NavmeshOffMeshKind.ManualOffMesh
         );
-
-        scene.InsertAABoxCollider
+        config.AddOffMeshConnection
         (
-            new AABB
-            {
-                Min = new(32.9f, 0.0f, -12.7f),
-                Max = new(41.4f, 0.0f, -0.6f)
-            },
-            SceneExtractor.PrimitiveFlags.ForceUnwalkable
-        );
-
-        scene.InsertAABoxCollider
-        (
-            new AABB
-            {
-                Min = new(34.1f, 0.0f, -0.5f),
-                Max = new(36.7f, 13.0f, -16.6f)
-            },
-            SceneExtractor.PrimitiveFlags.ForceUnwalkable
+            new(35.09059f, -9.536743E-07f, -6.1794167f),
+            new(38.55001f, -9.536743E-07f, 0.0067253113f),
+            0.5f,
+            false,
+            0,
+            NavmeshArea.ManualOffMesh,
+            NavmeshPolyFlags.ManualOffMesh,
+            NavmeshOffMeshKind.ManualOffMesh
         );
     }
 
     public override void CustomizeMesh(Navmesh mesh, List<uint> festivalLayers)
     {
-        // 传送带
-        LinkPoints(mesh, new(58.3f, -0.0f, -20.4f), new(58.0f, -9.0f, -41.8f));
-
-        // 进门前
-        LinkPoints(mesh, new(35.1f, 0.0f, -0.0f),  new(48.5f, 0.0f, 0.2f));
-        LinkPoints(mesh, new(29.9f, 0.0f, -11.1f), new(35.2f, 0.0f, 0.1f));
-        LinkPoints(mesh, new(35.3f, 0.0f, -6.0f),  new(40.2f, 0.0f, 0.5f));
-        LinkPoints(mesh, new(41.5f, 0.0f, 0.0f),   new(47.3f, 0.0f, 0.2f));
-
-        // 两个下落点
-        LinkDrop(mesh, new(21.6f, 36.0f, -13.3f), new(25.0f, 23.6f, -20.7f));
-        LinkDrop(mesh, new(17.9f, 18.0f, -17.5f), new(20.0f, 8.8f, -23.6f));
+        LinkDrop(mesh, new(20.97561f, 35.141724f, -15.054151f), new(24.470856f, 23.111866f, -19.177319f));
+        LinkPoints(mesh, new(38.509922f, -9.536743E-07f, -0.274802f), new(48.962902f, 0f, -0.06897486f));
+        LinkPoints(mesh, new(58.524216f, 0.011983752f, -21.014153f),  new(58.992924f, -9f, -42.920166f));
     }
-
-    public override void CustomizeSettings(DtNavMeshCreateParams config) =>
-        config.AddOffMeshConnection(new(35.1f, 0.0f, -0.0f), new(57.5f, 0.0f, 0.1f), 1f, true);
 }
