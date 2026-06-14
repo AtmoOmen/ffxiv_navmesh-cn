26d795ff33ac9a1805b0c232ccac3b005407ffa1
AtmoOmen <atmoomen2004@outlook.com>
2026-06-14 21:12:31 +0800

    Z0155库尔札斯中央高地

--- vnavmesh/Navigation/Custom/Customizations/Territory/Overworld/Z0155库尔札斯中央高地.cs
+++ vnavmesh/Navigation/Custom/Customizations/Territory/Overworld/Z0155库尔札斯中央高地.cs
@@ -9,7 +9,7 @@ namespace vnavmesh.Navigation.Custom.Customizations.Territory.Overworld;
 [CustomizationTerritory(155)]
 internal class Z0155库尔札斯中央高地 : NavmeshCustomization
 {
-    public override int Version => 8;
+    public override int Version => 9;
 
     public Z0155库尔札斯中央高地() =>
         ApplyAgentRadiusOneSettings();
@@ -25,5 +25,46 @@ internal class Z0155库尔札斯中央高地 : NavmeshCustomization
             foreach (var inst in tower.Instances)
                 inst.ForceSetPrimFlags |= SceneExtractor.PrimitiveFlags.ForceWalkable;
         }
+    }
+
+    public override void CustomizeMesh(Common.Navigation.Mesh.Runtime.Navmesh mesh, List<uint> festivalLayers)
+    {
+        // 白云崖前哨门洞
+        List<Vector3> doorwayWhitebrim =
+        [
+            new(-454.47195f, 211.4575f, -291.54233f),
+            new(-457.46005f, 211.4575f, -291.67108f)
+        ];
+
+        for (var i = 0; i < doorwayWhitebrim.Count - 1; i++)
+            LinkShortcut(mesh, doorwayWhitebrim[i], doorwayWhitebrim[i + 1], true);
+
+        // 阿德内尔占星台与巨龙首之间的门洞
+        List<Vector3> doorwayMartiallais =
+        [
+            new(198.35889f, 257f, 76.33306f),
+            new(205.2478f, 256.14615f, 83.13481f)
+        ];
+
+        for (var i = 0; i < doorwayMartiallais.Count - 1; i++)
+            LinkShortcut(mesh, doorwayMartiallais[i], doorwayMartiallais[i + 1], true);
+
+        // 巨龙首营地
+        List<Vector3> walkway =
+        [
+            new(279.8393f, 319f, -232.66096f),
+            new(279.70917f, 324.0579f, -225.59511f),
+            new(279.66046f, 324.0003f, -221.4777f),
+            new(279.6848f, 324.0004f, -200.20921f),
+            new(279.67496f, 324.0007f, -195.25887f),
+            new(279.564f, 324.0007f, -186.17705f),
+            new(279.57233f, 324.0007f, -182.04602f),
+            new(279.4925f, 324f, -159.71964f),
+            new(266.03177f, 324f, -156.23119f),
+            new(261.66354f, 323.969f, -156.22154f)
+        ];
+
+        for (var i = 0; i < walkway.Count - 1; i++)
+            LinkShortcut(mesh, walkway[i], walkway[i + 1], true);
     }
 }
