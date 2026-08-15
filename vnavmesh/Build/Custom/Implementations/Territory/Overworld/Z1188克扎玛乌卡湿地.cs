93946a785c93a7595d919ca6323fede62da51093
AtmoOmen <atmoomen2004@outlook.com>
2026-08-15 22:12:49 +0800

    Z1188克扎玛乌卡湿地

--- vnavmesh/Build/Custom/Implementations/Territory/Overworld/Z1188克扎玛乌卡湿地.cs
+++ vnavmesh/Build/Custom/Implementations/Territory/Overworld/Z1188克扎玛乌卡湿地.cs
@@ -1,13 +1,16 @@
+using DotRecast.Detour;
 using vnavmesh.Build.Custom.Abstractions;
 using vnavmesh.Build.Custom.Attributes;
+using vnavmesh.Build.Custom.Extensions;
 using vnavmesh.Build.Scene;
+using vnavmesh.Common.Build.Ground;
 
 namespace vnavmesh.Build.Custom.Implementations.Territory.Overworld;
 
 [CustomizationTerritory(1188)]
 internal class Z1188克扎玛乌卡湿地 : NavmeshCustomization
 {
-    public override int Version => 1;
+    public override int Version => 2;
 
     public Z1188克扎玛乌卡湿地() =>
         ApplyAgentRadiusOneSettings();
@@ -21,4 +24,18 @@ internal class Z1188克扎玛乌卡湿地 : NavmeshCustomization
         if (scene.Meshes.TryGetValue("bg/ex5/02_ykt_y6/fld/y6f2/collision/y6f2_x0_tst00.pcb", out var mesh))
             mesh.Instances[0].WorldTransform.Row3.Y += 0.05f;
     }
+    
+    public override void CustomizeSettings(DtNavMeshCreateParams config) =>
+        // 奸臣的展示台
+        config.AddOffMeshConnection
+        (
+            new(379.16513f, 132.05937f, 686.66486f),
+            new(378.89407f, 132.15149f, 693.27045f),
+            0.5f,
+            false,
+            0,
+            NavmeshArea.ManualOffMesh,
+            NavmeshPolyFlags.ManualOffMesh,
+            NavmeshOffMeshKind.ManualOffMesh
+        );
 }
